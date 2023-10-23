529f1d07df01c1b17149ef81672b2e5df7cb1452
fdsantoz <147438578+fdsantoz@users.noreply.github.com>
2023-10-23 14:16:23 -0300

    Add files via upload

---
+++ douglas.py
@@ -0,0 +1,5 @@
+nota1 = float(input("digite a nota 01"))
+nota2 = float(input("digite a nota 02"))
+nota3 = float(input("digite a nota 03"))
+media = (nota1 + nota2 + nota3) / 3
+print("sua média é: ", media)
